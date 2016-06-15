f477d30739d83bb8f9e0192cac66a2db32781b3b
Sven A. Schmidt <sas@finestructure.co>
2016-06-15 16:58:59 +0100

    Complete test working

--- Tests/DiffMatchPatch/DiffMatchPatchTests.swift
+++ Tests/DiffMatchPatch/DiffMatchPatchTests.swift
@@ -3,39 +3,34 @@ import XCTest
 
 // Test helpers to keep Nimble format
 
-struct Matcher<T: Comparable> {
+struct Matcher<T: Equatable> {
     let value: T
 }
 
-func ==<T: Comparable> (lhs: Matcher<T>, rhs: Matcher<T>) {
+func ==<T: Equatable> (lhs: Matcher<T>, rhs: Matcher<T>) {
     XCTAssertEqual(lhs.value, rhs.value)
 }
 
-func ==<T: Comparable> (lhs: Matcher<T>, rhs: T) {
+func ==<T: Equatable> (lhs: Matcher<T>, rhs: T) {
     XCTAssertEqual(lhs.value, rhs)
 }
 
-func expect<T: Comparable>(_ value: T) -> Matcher<T> {
+func expect<T: Equatable>(_ value: T) -> Matcher<T> {
     return Matcher(value: value)
 }
 
-// make Operation Comparable
-func == (lhs: Operation, rhs: Operation) -> Bool {
-    return lhs == rhs
-}
-
 class DiffMatchPatchTests: XCTestCase {
+    
     func test_computeDiff() {
         let d = computeDiff(a: "foo2bar", b: "foobar")
         expect(d.count) == 3
-        // expect(d[0].operation) == Operation.DiffEqual
+        expect(d[0].operation) == .diffEqual
         expect(d[0].text) == "foo"
-        // expect(d[1].operation) == Operation.DiffDelete
+        expect(d[1].operation) == .diffDelete
         expect(d[1].text) == "2"
-        // expect(d[2].operation) == Operation.DiffEqual
+        expect(d[2].operation) == .diffEqual
         expect(d[2].text) == "bar"
     }
-
 
     static var allTests : [(String, (DiffMatchPatchTests) -> () throws -> Void)] {
         return [
